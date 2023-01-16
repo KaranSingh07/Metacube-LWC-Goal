522f7e813e66d59212bdcd40e1ecc1b8ea00f474
Karan Singh Solanki <karan.solanki@metacube.com>
2023-01-16 12:51:57 +0530

    Moved async operation to populate teams into common component teams

--- force-app/main/default/lwc/teams/teams.js
+++ force-app/main/default/lwc/teams/teams.js
@@ -1,12 +1,58 @@
 import { LightningElement, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { LABELS } from './labels';
+import getTeams from '@salesforce/apex/TeamController.getTeams';
 
 export default class Teams extends LightningElement {
 	@track state = {
 		teams: [],
+		isLoading: true,
+		isError: false,
 	};
 
-	handleUpdateTeams(event) {
-		this.state.teams = event.detail.teams;
+	async connectedCallback() {
+		this.showSpinner(true);
+		await this.populateTeamOptions();
+	}
+
+	async populateTeamOptions() {
+		await getTeams()
+			.then((data) => {
+				this.createTeamOptions(data);
+			})
+			.catch((error) => {
+				this.state.isError = true;
+				this.showErrorToast(LABELS.ToastErrorTitle, JSON.stringify(error));
+			})
+			.finally(() => {
+				this.showSpinner(false);
+			});
+	}
+
+	createTeamOptions(teamData) {
+		let teamOptions = [];
+		teamData.forEach((team) => {
+			teamOptions.push(this.createPicklistOption(team.Name, team.Id));
+		});
+		this.state.teams = teamOptions;
+	}
+
+	createPicklistOption(label, value) {
+		return { label: label, value: value };
+	}
+
+	showSpinner(value) {
+		this.state.isLoading = value;
+	}
+
+	showErrorToast(title, message) {
+		this.dispatchEvent(
+			new ShowToastEvent({
+				title: title,
+				message: message,
+				variant: 'error',
+			})
+		);
 	}
 
 	handleSyncData() {
@@ -16,5 +62,9 @@ export default class Teams extends LightningElement {
 
 	get teams() {
 		return this.state.teams;
+	}
+
+	get showContent() {
+		return !(this.state.isLoading || this.state.isError);
 	}
 }
