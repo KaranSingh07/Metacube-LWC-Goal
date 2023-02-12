004b3d4d98effa47db8bd1bb9afa43fc55de092b
Karan Singh Solanki <karan.solanki@metacube.com>
2023-02-12 20:01:13 +0530

    Added jest test to verify error toast and sync data is not called when failed to createRecord

--- force-app/main/default/lwc/teamsCreateMember/__tests__/teamsCreateMember.test.js
+++ force-app/main/default/lwc/teamsCreateMember/__tests__/teamsCreateMember.test.js
@@ -146,4 +146,51 @@ describe('TeamsCreateMember Component', () => {
 		expect(mockToastHandler).toHaveBeenCalled();
 		expect(mockSyncDataHandler).toHaveBeenCalled();
 	});
+
+	it('Should show error toast message when record save is failed and do not dispatch syncdata event', async () => {
+		// Given
+		const component = createComponent();
+		component.teams = mockTeams;
+		createRecord.mockRejectedValue({});
+
+		document.body.appendChild(component);
+
+		const mockToastHandler = jest.fn();
+		const mockSyncDataHandler = jest.fn();
+		component.addEventListener(LIGHTNING_SHOWTOAST_EVENT_NAME, mockToastHandler);
+		component.addEventListener('syncdata', mockSyncDataHandler);
+
+		await flushPromises();
+
+		const memberNameInput = component.shadowRoot.querySelector(
+			'lightning-input[data-id="memberName"]'
+		);
+		const memberSkillsInput = component.shadowRoot.querySelector(
+			'lightning-input[data-id="memberSkills"]'
+		);
+		const memberTeamCombobox = component.shadowRoot.querySelector(
+			'lightning-combobox[data-id="memberTeam"]'
+		);
+
+		memberNameInput.dispatchEvent(
+			new CustomEvent('change', { detail: { value: mockFields.Name } })
+		);
+		memberSkillsInput.dispatchEvent(
+			new CustomEvent('change', { detail: { value: mockFields.Skills__c } })
+		);
+		memberTeamCombobox.dispatchEvent(
+			new CustomEvent('change', { detail: { value: mockFields.Team__c } })
+		);
+
+		// When
+		const createMemberButton = component.shadowRoot.querySelector(
+			'lightning-button[title="createMember"]'
+		);
+		createMemberButton.click();
+		await flushPromises();
+
+		// Then
+		expect(mockToastHandler).toHaveBeenCalled();
+		expect(mockSyncDataHandler).not.toHaveBeenCalled();
+	});
 });
